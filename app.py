a3e0a9ee609d78fd46d39d8fa969886350817d6d
if <if@>
2026-01-07 22:23:15 +0300

    upd controls styles

--- app.py
+++ app.py
@@ -133,7 +133,7 @@ loop()
 
 @st.fragment
 def controls() -> None:
-    cols = st.columns(2, gap="small")
+    cols = st.columns(4)
     with cols[0]:
         if st.button("←", use_container_width=True, help="Turn left"):
             st.session_state.left = True
